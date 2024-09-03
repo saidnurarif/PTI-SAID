238c93850e00ec6c052b87fc2c58d4a38ea600cb
saidnurarif <saidarif217@gmail.com>
2024-09-03 12:35:04 +0700

    Perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,19 @@
+# Simulasi hacking situs pemerintah
+a = 1
+b = 1
+c = a*b
+
+# operasi  ( operation ) 
+perkalian = a*b
+pembagian = a/b
+pengurangan = a-b
+modulo = a%b
+pangkat = a**b
+
+print (perkalian)
+print (pembagian)
+print (pengurangan)
+print (pangkat)
+print (modulo)
+
+print("Hacker KOMINFO")
